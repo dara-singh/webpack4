d4034c2e053b15ec850b20439ea848d7b67279d7
dara-singh <88dara@gmail.com>
2018-06-06 19:39:45 +0530

    Hot Module Replacement

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 *****************************************/
 
 const path = require('path');
+ const webpack = require('webpack');
 
 /***************************************
 * Entry
@@ -12,13 +13,17 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   devtool: 'inline-source-map',
   devServer: {
-     contentBase: './dist'
+     contentBase: './dist',
+     hot: true
   },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
